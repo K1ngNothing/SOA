b948dda167f457a014a2c1a7915fd5c282e9ed76
K1ngNothing <dartvaderproskurin@gmail.com>
2024-04-27 18:30:15 +0300

    Fixed main-service API: now it uses proper HTTP methods + Fixed /get-posts semantic

--- services/main-service/main-service.py
+++ services/main-service/main-service.py
@@ -1,17 +1,18 @@
 # Flask
-from flask import Flask, jsonify, request
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-# SQLAlchemy
-from sqlalchemy.exc import IntegrityError
+from flask import Flask, request
 from werkzeug.exceptions import (
     NotFound,
     Unauthorized,
     Conflict,
     Forbidden,
-    InternalServerError
+    InternalServerError,
+    BadRequest
 )
+
+# SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
+from flask_migrate import Migrate
 
 # gRPC
 import grpc
@@ -34,6 +35,7 @@ public_key = ""
 token_ttl_hours = 1
 
 
+# DB
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
@@ -45,6 +47,7 @@ class User(db.Model):
     phone_number = db.Column(db.String(20), nullable=True, unique=True)
 
 
+# JWT shenanigans
 def encode_jwt(user_id):
     expiration_time = datetime.now() + timedelta(hours=token_ttl_hours)
     return jwt.encode(
@@ -55,6 +58,8 @@ def encode_jwt(user_id):
 
 
 def decode_jwt(token):
+    if not token:
+        raise Unauthorized('Token is missing')
     try:
         payload = jwt.decode(token, public_key, algorithms=['RS256'])
         return payload['user_id']
@@ -70,6 +75,7 @@ def hash_password(username, password):
     return hashed_password
 
 
+# Start server
 def load_keys():
     global private_key, public_key
     private_key_path = os.environ.get('PRIVATE_KEY_PATH')
@@ -87,81 +93,94 @@ def setup_post_service():
     channel = grpc.insecure_channel(post_service_addr)
     post_service_stub = post_service_pb2_grpc.PostServiceStub(channel)
 
+# ----- Helpers -----
+
+
+def get_header(request, header: str):
+    res = request.headers.get(header)
+    if not res:
+        raise BadRequest(f'No {header} passed')
+    return res
+
+
+def get_post_id(request):
+    return get_header(request, 'post_id')
+
+
 # ----- Routes -----
 
 
 @app.route('/register', methods=['POST'])
 def register():
-    data = request.json
-    username = data.get('username')
-    password = data.get('password')
+    request_body = request.json
+    username = request_body.get('username')
+    password = request_body.get('password')
 
     try:
         new_user = User(username=username,
                         password=hash_password(username, password))
         db.session.add(new_user)
         db.session.commit()
-        return jsonify({'message': 'User has been registered successfully'})
+        return {'message': 'User has been registered successfully'}
     except IntegrityError:
         raise Conflict('User already exists')
 
 
 @app.route('/login', methods=['POST'])
 def login():
-    data = request.json
-    username = data.get('username')
-    password = data.get('password')
+    request_body = request.json
+    username = request_body.get('username')
+    password = request_body.get('password')
 
     user = User.query.filter_by(
         username=username, password=hash_password(username, password)).first()
     if not user:
         raise Forbidden('Invalid username or password')
 
-    return jsonify({
+    return {
         'auth_token': encode_jwt(user.id),
-        'message': 'Authentication successful! Token is granted'
-    })
+        'message': 'Authentication successful, token is granted'
+    }
 
 
-@app.route('/update-user-info', methods=['PUT'])
+@ app.route('/update-user-info', methods=['PUT'])
 def update_user_info():
-    data = request.json
-    auth_token = data.get('auth_token')
-    user_id = decode_jwt(auth_token)
+    user_id = decode_jwt(request.headers.get('Authorization'))
+    request_body = request.json
 
     user = User.query.filter_by(id=user_id).first()
-    user.first_name = data.get('first_name', user.first_name)
-    user.last_name = data.get('last_name', user.last_name)
-    user.date_of_birth = data.get('date_of_birth', user.date_of_birth)
-    user.email = data.get('email', user.email)
-    user.phone_number = data.get('phone_number', user.phone_number)
+    user.first_name = request_body.get('first_name', user.first_name)
+    user.last_name = request_body.get('last_name', user.last_name)
+    user.date_of_birth = request_body.get('date_of_birth', user.date_of_birth)
+    user.email = request_body.get('email', user.email)
+    user.phone_number = request_body.get('phone_number', user.phone_number)
 
     db.session.commit()
-    return jsonify({'message': "User's personal info successfully updated"})
+    return {'message': "User's personal info successfully updated"}
 
 
-@app.route('/create-post', methods=['POST'])
+@ app.route('/create-post', methods=['POST'])
 def create_post():
-    data = request.json
-    user_id = decode_jwt(data.get('auth_token'))
-    content = data.get('content')
+    user_id = decode_jwt(request.headers.get('Authorization'))
+    request_body = request.json
+    content = request_body.get('content')
 
     grpc_request = post_service_pb2.CreatePostRequest(
         author_id=user_id, content=content)
     response = post_service_stub.CreatePost(grpc_request)
 
-    return jsonify({
+    return {
         'post_id': response.post_id,
         'message': 'Post created successfully!'
-    })
+    }
 
 
 @app.route('/update-post', methods=['PUT'])
 def update_post():
-    data = request.json
-    user_id = decode_jwt(data.get('auth_token'))
-    post_id = data.get('post_id')
-    content = data.get('content')
+    user_id = decode_jwt(request.headers.get('Authorization'))
+    post_id = get_post_id(request)
+    request_body = request.json
+    content = request_body.get('content')
 
     try:
         grpc_request = post_service_pb2.UpdatePostRequest(
@@ -170,9 +189,7 @@ def update_post():
             content=content
         )
         response = post_service_stub.UpdatePost(grpc_request)
-        return jsonify({
-            'message': 'Post updated successfully!'
-        })
+        return {'message': 'Post updated successfully!'}
     except grpc.RpcError as e:
         status_code = e.code()
         error_message = e.details()
@@ -183,11 +200,10 @@ def update_post():
         raise InternalServerError()
 
 
-@app.route('/delete-post', methods=['PUT'])
+@app.route('/delete-post', methods=['DELETE'])
 def delete_post():
-    data = request.json
-    user_id = decode_jwt(data.get('auth_token'))
-    post_id = data.get('post_id')
+    user_id = decode_jwt(request.headers.get('Authorization'))
+    post_id = get_post_id(request)
 
     try:
         grpc_request = post_service_pb2.DeletePostRequest(
@@ -196,9 +212,7 @@ def delete_post():
         )
         response = post_service_stub.UpdatePost(grpc_request)
         if response.success:
-            return jsonify({
-                'message': 'Post deleted successfully!'
-            })
+            return {'message': 'Post deleted successfully!'}
     except grpc.RpcError as e:
         status_code = e.code()
         error_message = e.details()
@@ -209,11 +223,10 @@ def delete_post():
         raise InternalServerError()
 
 
-@app.route('/get-post', methods=['PUT'])
+@app.route('/get-post', methods=['GET'])
 def get_post():
-    data = request.json
-    user_id = decode_jwt(data.get('auth_token'))
-    post_id = data.get('post_id')
+    user_id = decode_jwt(request.headers.get('Authorization'))
+    post_id = get_post_id(request)
 
     try:
         grpc_request = post_service_pb2.GetPostByIdRequest(
@@ -222,10 +235,10 @@ def get_post():
         )
         response = post_service_stub.GetPostById(grpc_request)
 
-        return jsonify({
+        return {
             'author': response.author_id,
             'content': response.content,
-        })
+        }
     except grpc.RpcError as e:
         status_code = e.code()
         error_message = e.details()
@@ -236,35 +249,27 @@ def get_post():
         raise InternalServerError()
 
 
-@app.route('/get-posts', methods=['PUT'])
+@app.route('/get-posts', methods=['GET'])
 def get_posts():
-    k_page_size = 1
-
-    data = request.json
-    user_id = decode_jwt(data.get('auth_token'))
-    posts_read = 0
+    user_id = decode_jwt(request.headers.get('Authorization'))
+    posts_read = int(get_header(request, 'starting-post'))
+    k_page_size = int(get_header(request, 'want-to-read'))
     posts = []
-    while True:
-        grpc_request = post_service_pb2.GetPostsRequest(
-            user_id=user_id,
-            posts_read=posts_read,
-            want_to_read=k_page_size,
-        )
-        response = post_service_stub.GetPosts(grpc_request)
-        posts_read += len(response.posts)
-        if len(response.posts) == 0:
-            break
 
-        for post in response.posts:
-            posts.append({
-                'post_id': post.post_id,
-                'author': post.author_id,
-                'content': post.content,
-            })
+    grpc_request = post_service_pb2.GetPostsRequest(
+        user_id=user_id,
+        posts_read=posts_read,
+        want_to_read=k_page_size,
+    )
+    response = post_service_stub.GetPosts(grpc_request)
+    for post in response.posts:
+        posts.append({
+            'post_id': post.post_id,
+            'author': post.author_id,
+            'content': post.content,
+        })
 
-    return jsonify({
-        'posts': posts,
-    })
+    return {'posts': posts}
 
 
 def serve():
